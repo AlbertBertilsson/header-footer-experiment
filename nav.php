b30a75500487540ddd233b21369200673ef97986
Albert Bertilsson <albert.bertilsson@ikea.com>
2017-09-24 10:51:05 +0200

    Add menu

---
+++ nav.php
@@ -0,0 +1,32 @@
+<?php
+	header('Cache-Control: max-age=60');
+	sleep(5);
+?>
+<html>
+<head>
+  <title>Test</title>
+</head>
+<body>
+	<?php include "header.html"; ?>
+
+<style>
+#close {
+	display: inline;
+}
+#menu {
+	display: none;
+}
+</style>
+
+<h1>Navigation</h1>
+<b>
+	Products<br>
+</b>
+Small products<br>
+Big products<br>
+<b>About</b>
+Stores<br>
+History
+
+</body>
+</html>
