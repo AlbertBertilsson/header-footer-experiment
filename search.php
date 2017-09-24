bc3cef8a11b6f3606609d06379e2af226316e57d
Albert Bertilsson <albert.bertilsson@ikea.com>
2017-09-24 20:17:56 +0200

    Added search page

---
+++ search.php
@@ -0,0 +1,41 @@
+<?php
+    header('Cache-Control: max-age=60');
+?>
+<html>
+<head>
+  <title>Navigation</title>
+  <?php include "head.html"; ?>
+</head>
+<body>
+    <?php include "header.html"; ?>
+
+
+    
+<style>
+#close {
+    display: inline;
+}
+
+.history a {
+    background-color: #666;
+    color: white;
+    border-radius: 2px;
+    padding: 5px;
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-left: 15px;
+    text-decoration: none;
+    line-height: 40px;
+}
+</style>
+
+<script>
+document.getElementById("search-input").focus();
+</script>
+
+<h1>Search</h1>
+
+<div class="history">
+<b>Search history:</b>
+<a href="">Billy</a> <a href="">Bokhylla</a> <a href="">Hyllor</a> <a href="">Böcker</a> <a href="">bakhylla</a> <a href="">Hall hylla</a>
+</div>
